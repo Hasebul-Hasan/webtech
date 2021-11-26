96e4e897b7c1bd328639c6a7f5a2354e35850fdb
Hasebul-Hasan <79850756+Hasebul-Hasan@users.noreply.github.com>
2021-11-26 12:04:09 -0600

    Add files via upload

---
+++ digital wallet/tansaction-history.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Page 3 [Tansaction History]</title>
+</head>
+<body>
+	<h1>Digital Wallet</h1>
+	<form action="tansaction-historyAction.php" method="post">
+	<a href="Home.php">1.Home</a>
+	<a href="add-beneficiary.php">2.Add Beneficiary</a>
+	<a href="tansaction-history".php>3.Tansaction History</a>
+	<br><br>
+
+	<lable>From:</lable>
+    <input type="date" name="from">
+
+    <lable>To:</lable>
+    <input type="date" name="to">
+
+    <br><br>
+    <input type="submit" name="search">
+
+    	
+
+</from>
+</body>
+</html>
